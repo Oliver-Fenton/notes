8269e62462933819478b0863ecc2a9ff4a59d014
Gillian Johnson <gillian.johnson@zemind.ca>
2023-02-10 15:22:29 -0500

    Edited Main file

--- application/src/main/kotlin/notes/application/Main.kt
+++ application/src/main/kotlin/notes/application/Main.kt
@@ -2,19 +2,15 @@ package notes.application
 
 import javafx.application.Application
 import javafx.scene.Scene
-import javafx.scene.control.Label
 import javafx.scene.layout.BorderPane
-import javafx.scene.layout.StackPane
 import javafx.stage.Stage
-import notes.shared.SysInfo
-import notes.model.Model
 
 class Main : Application() {
     override fun start(stage: Stage) {
-        stage.scene = Scene(
-            BorderPane(Label(Model.testMessage)),
-            250.0,
-            150.0)
+        val borderPaneLayout = BorderPane()
+//        borderPaneLayout.top = Toolbar()
+
+        stage.scene = Scene(borderPaneLayout, 250.0, 150.0)
         stage.isResizable = true
         stage.minWidth = 600.0
         stage.minHeight = 400.0
