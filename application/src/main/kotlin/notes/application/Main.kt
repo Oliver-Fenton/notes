b22795c68ce76b76eddf646bbe3f6df37ddec726
lindsaymark <65431025+lindsaymark@users.noreply.github.com>
2023-02-10 19:31:33 -0500

    Basic menu setup, Quit is functional

--- application/src/main/kotlin/notes/application/Main.kt
+++ application/src/main/kotlin/notes/application/Main.kt
@@ -1,14 +1,21 @@
 package notes.application
 
 import javafx.application.Application
+import javafx.application.Platform
 import javafx.scene.Scene
+import javafx.scene.control.Menu
+import javafx.scene.control.MenuBar
+import javafx.scene.control.MenuItem
+import javafx.scene.input.KeyCodeCombination
 import javafx.scene.layout.BorderPane
+import javafx.scene.layout.VBox
 import javafx.stage.Stage
 
 class Main : Application() {
     override fun start(stage: Stage) {
         val borderPaneLayout = BorderPane()
-//        borderPaneLayout.top = Toolbar()
+       // val topVBox = VBox(Menubar(), Toolbar())
+        borderPaneLayout.top = Menubar()
 
         stage.scene = Scene(borderPaneLayout, 250.0, 150.0)
         stage.isResizable = true
